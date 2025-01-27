ef126b678fdfbed554c6ef6ac94f231bfc971d0b
Alex <beauchama@kappaduck.com>
2025-01-26 23:10:59 -0500

    Added base SDL exception to manage errors

---
+++ src/KappaDuck.Aquila/Exceptions/SDLException.cs
@@ -0,0 +1,54 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace KappaDuck.Aquila.Exceptions;
+
+/// <summary>
+/// Represents an exception that is thrown when an SDL error occurs.
+/// It uses <see cref="SDL.GetError"/> to get the error message
+/// then <see href="https://wiki.libsdl.org/SDL3/SDL_ClearError">SDL_ClearError</see> to clear the message.
+/// </summary>
+public sealed partial class SDLException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SDLException"/> class.
+    /// </summary>
+    public SDLException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SDLException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message from SDL or a custom message.</param>
+    public SDLException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SDLException"/> class
+    /// with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message from SDL or a custom message.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public SDLException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    [DoesNotReturn]
+    internal static void Throw()
+    {
+        string message = SDL.GetError();
+        SDL_ClearError();
+
+        throw new SDLException(message);
+    }
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial void SDL_ClearError();
+}
