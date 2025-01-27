056a980dfe0ad17a09a3f6051f2a2260d150a1bc
Alex <beauchama@kappaduck.com>
2025-01-26 22:42:00 -0500

    Added initialization and shutdown for SDL subsystems

--- src/Aquila.Demo/Program.cs
+++ src/Aquila.Demo/Program.cs
@@ -1,4 +1,11 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using KappaDuck.Aquila;
+using KappaDuck.Aquila.System;
+
+SDL.Init(SubSystem.Video);
+
 Console.WriteLine("Hello, Aquila!");
+
+SDL.Quit();
