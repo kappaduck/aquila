31636877fd65a37de604eafd20708d49ae817486
beauchama <beauchama@kappaduck.com>
2025-01-27 19:57:43 -0500

    Added OpenUrl

--- src/KappaDuck.Aquila/SDL.cs
+++ src/KappaDuck.Aquila/SDL.cs
@@ -55,6 +55,27 @@ public static partial class SDL
     }
 
     /// <summary>
+    /// Open a URL/URI in the browser or other appropriate external application.
+    /// </summary>
+    /// <param name="uri">The URL/URI to open.</param>
+    /// <remarks>
+    /// Open a URL in a separate, system-provided application. How this works will vary wildly depending on the platform.
+    /// This will likely launch what makes sense to handle a specific URL's protocol (a web browser for http://, etc),
+    /// but it might also be able to launch file managers for directories and other things.
+    /// What happens when you open a URL varies wildly as well: your game window may lose
+    /// focus(and may or may not lose focus if your game was Fullscreen or grabbing input at the time).
+    /// On mobile devices, your app will likely move to the background or your process might be paused. Any given platform may or may not handle a given URL.
+    /// If this is unimplemented (or simply unavailable) for a platform, this will fail with an error.
+    /// A successful result does not mean the URL loaded, just that we launched something to handle it(or at least believe we did).
+    /// All this to say: this function can be useful, but you should definitely test it on every platform you target.
+    /// </remarks>
+    /// <returns>True on success of false on failure; call <see cref="GetError"/> for more information.</returns>
+    public static bool OpenUrl(Uri uri) => SDL_OpenURL(uri.ToString());
+
+    /// <inheritdoc cref="OpenUrl(Uri)"/>
+    public static bool OpenUrl(string url) => SDL_OpenURL(url);
+
+    /// <summary>
     /// Clean up all initialized subsystems.
     /// </summary>
     /// <remarks>
@@ -103,6 +124,11 @@ public static partial class SDL
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static partial bool SDL_InitSubSystem(SubSystem subSystem);
+
+    [LibraryImport(NativeLibrary, StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool SDL_OpenURL(string url);
 
     [LibraryImport(NativeLibrary)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
