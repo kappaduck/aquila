022b0780ddd72f69502a86267ddd072912e6ea15
beauchama <beauchama@kappaduck.com>
2025-02-03 21:19:54 -0500

    Added other missing methods for display

--- src/KappaDuck.Aquila/Video/Displays/Display.cs
+++ src/KappaDuck.Aquila/Video/Displays/Display.cs
@@ -2,6 +2,7 @@
 // The source code is licensed under MIT License.
 
 using KappaDuck.Aquila.Exceptions;
+using KappaDuck.Aquila.Geometry;
 using KappaDuck.Aquila.Marshallers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -27,6 +28,55 @@ public sealed partial class Display
     /// Gets the name of the display or <see cref="string.Empty"/> on failure; call <see cref="SDL.GetError"/> for more information.
     /// </summary>
     public string Name => SDL_GetDisplayName(Id);
+
+    /// <summary>
+    /// Gets the bounds of the display.
+    /// </summary>
+    /// <exception cref="SDLException">An error occurred while getting the display bounds.</exception>
+    public Rectangle<int> Bounds
+    {
+        get
+        {
+            if (SDL_GetDisplayBounds(Id, out Rectangle<int> bounds) == 0)
+                SDLException.Throw();
+
+            return bounds;
+        }
+    }
+
+    /// <summary>
+    /// Gets the usable desktop area represented by a display, in screen coordinates.
+    /// </summary>
+    /// <remarks>
+    /// This is the same area as <see cref="Bounds"/> reports, but with portions reserved by the system removed.
+    /// </remarks>
+    public Rectangle<int> UsableBounds
+    {
+        get
+        {
+            if (SDL_GetDisplayUsableBounds(Id, out Rectangle<int> bounds) == 0)
+                SDLException.Throw();
+
+            return bounds;
+        }
+    }
+
+    /// <summary>
+    /// Gets content scale of the display or 0.0f on failure; call <see cref="SDL.GetError"/> for more information.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The content scale is the expected scale for content based on the DPI settings of the display.
+    /// For example, a 4K display might have a 2.0 (200%) display scale,
+    /// which means that the user expects UI elements to be twice as big on this display, to aid in readability.
+    /// </para>
+    /// <para>
+    /// After window creation, SDL_GetWindowDisplayScale() should be used to query the content scale factor for individual windows
+    /// instead of querying the display for a window and calling this function, as the per-window content scale factor may differ
+    /// from the base value of the display it is on, particularly on high-DPI and/or multi-monitor desktop configurations.
+    /// </para>
+    /// </remarks>
+    public float ContentScale => SDL_GetDisplayContentScale(Id);
 
     /// <summary>
     /// Gets information about the current display mode.
@@ -82,6 +132,38 @@ public sealed partial class Display
         SDL.Free(modes);
 
         return displayModes;
+    }
+
+    /// <summary>
+    /// Get the display containing the specified point.
+    /// </summary>
+    /// <param name="point">The point to query.</param>
+    /// <returns>The display containing the specified point.</returns>
+    /// <exception cref="SDLException">Failed to get the display containing the specified point.</exception>
+    public static unsafe Display GetDisplay(Point<int> point)
+    {
+        uint display = SDL_GetDisplayForPoint(&point);
+
+        if (display == uint.MinValue)
+            SDLException.Throw();
+
+        return new Display(display);
+    }
+
+    /// <summary>
+    /// Get the display containing the specified rectangle.
+    /// </summary>
+    /// <param name="rectangle">The rectangle to query.</param>
+    /// <returns>The display entirely containing the specified rectangle or closest to the center of the rectangle.</returns>
+    /// <exception cref="SDLException">Failed to get the display containing the specified rectangle.</exception>
+    public static unsafe Display GetDisplay(Rectangle<int> rectangle)
+    {
+        uint display = SDL_GetDisplayForRect(&rectangle);
+
+        if (display == uint.MinValue)
+            SDLException.Throw();
+
+        return new Display(display);
     }
 
     /// <summary>
@@ -142,12 +224,32 @@ public sealed partial class Display
 
     [LibraryImport(SDL.NativeLibrary)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial byte SDL_GetDisplayBounds(uint display, out Rectangle<int> bounds);
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial byte SDL_GetDisplayUsableBounds(uint display, out Rectangle<int> bounds);
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial float SDL_GetDisplayContentScale(uint display);
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalUsing(typeof(OwnedStringMarshaller))]
     private static partial string SDL_GetDisplayName(uint display);
 
     [LibraryImport(SDL.NativeLibrary)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     private static partial uint SDL_GetDisplayProperties(uint display);
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static unsafe partial uint SDL_GetDisplayForPoint(Point<int>* point);
+
+    [LibraryImport(SDL.NativeLibrary)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static unsafe partial uint SDL_GetDisplayForRect(Rectangle<int>* rectangle);
 
     [LibraryImport(SDL.NativeLibrary)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
