4f337d366af865cb2c1985aa228c6e2d2377bdeb
beauchama <beauchama@kappaduck.com>
2025-02-03 20:42:45 -0500

    Added extensions

--- src/KappaDuck.Aquila/Geometry/Math.cs
+++ src/KappaDuck.Aquila/Geometry/Math.cs
@@ -5,6 +5,9 @@ namespace KappaDuck.Aquila.Geometry;
 
 internal static class Math
 {
+    /// <summary>
+    /// The machine epsilon for <see cref="float"/>. SDL3 uses it.
+    /// </summary>
     internal const float Epsilon = 1.192092896e-07f;
 
     internal static bool Approximately(float a, float b) => float.Abs(a - b) < Epsilon;
