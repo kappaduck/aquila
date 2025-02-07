316b69455b8495aa4b10bd19381f3ff18e3d8acf
beauchama <beauchama@kappaduck.com>
2025-02-07 17:35:01 -0500

    Updated SDLException

--- src/KappaDuck.Aquila/Exceptions/SDLException.cs
+++ src/KappaDuck.Aquila/Exceptions/SDLException.cs
@@ -1,18 +1,17 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using KappaDuck.Aquila.Interop;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace KappaDuck.Aquila.Exceptions;
 
 /// <summary>
 /// Represents an exception that is thrown when an SDL error occurs.
-/// It uses <see cref="SDL.GetError"/> to get the error message
+/// It uses <see href="https://wiki.libsdl.org/SDL3/SDL_GetError">SDL_GetError</see> to get the error message
 /// then use <see href="https://wiki.libsdl.org/SDL3/SDL_ClearError">SDL_ClearError</see> to clear the message.
 /// </summary>
-public sealed partial class SDLException : Exception
+public sealed class SDLException : Exception
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="SDLException"/> class.
@@ -39,16 +38,18 @@ public sealed partial class SDLException : Exception
     {
     }
 
+    internal static void ThrowIf([DoesNotReturnIf(true)] bool condition)
+    {
+        if (condition)
+            Throw();
+    }
+
     [DoesNotReturn]
     internal static void Throw()
     {
-        string message = SDL.GetError();
-        SDL_ClearError();
+        string message = Native.SDL_GetError();
+        Native.SDL_ClearError();
 
         throw new SDLException(message);
     }
-
-    [LibraryImport(SDL.NativeLibrary)]
-    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    private static partial void SDL_ClearError();
 }
